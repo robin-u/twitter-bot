44bba19397c7b9a5815437f847f1f6f9877e0ae1
Robin <robinundap@gmail.com>
2021-11-20 12:20:16 +0800

    added forwarding dm with an attachment feature

--- app.py
+++ app.py
@@ -2,28 +2,62 @@ from bot import Twbot
 import time
 
 tw = Twbot()
-# tw.auth_test() # to check if the credentials is valid
+credential_check = tw.auth_test() # to check if the credentials is valid
 
 def start():
+    """ Trigger word is "ftw!" """
+    trigger_word = 'ftw!'
     while True:
-        # read the dm
         messages = tw.read_dm()
         try:
             dm = messages[0]['message']
             dm_id = messages[0]['message_id']
+            media_id = messages[0]['media_id']
+            media_url = messages[0]['media_url']
+            media_short_url = messages[0]['short_url']
+            media_type = messages[0]['media_type']
         except:
-            print('Currently, there\'s no dm')
-            time.sleep(60)
+            print("Currently, there's no dm")
+            time.sleep(120)
             continue
-
-        # post the dm
-        if 'hello' in dm.lower():
-            tw.post_tweet(dm)
-            time.sleep(5)
-
-        # delete the posted dm
-        tw.delete_dm(dm_id)
-        time.sleep(30)
         
-if __name__ == '__main__':
+        # Check if the length is <= 280 and if there's trigger word
+        if len(dm) <= 280 and trigger_word in dm:
+            # Tweets if there's no media id and media type
+            if media_id is None and media_type is None:
+                print("DM doesn't have any media...")
+                tw.post_tweet(dm)
+                tw.delete_dm(dm_id)
+                time.sleep(60)
+            # Delete the DM because it has invalid attachment (gif or video)
+            elif media_id is None and media_type != 'photo':
+                print("File is not a photo. File type is %s. Please, attach a photo" % (media_type))
+                print("Wrong attachment type. Will delete DM")
+                tw.delete_dm(dm_id)
+                time.sleep(60)
+            else:
+                print("DM has a media attached...")
+                # Uploading media
+                media_ids = list()
+                media_ids.append(tw.upload_image(media_url))
+
+                # Tweets
+                dm = dm.replace(media_short_url, '')
+                tw.post_tweet_with_media(dm, media_ids[:], media_short_url)
+                tw.delete_dm(dm_id)
+                print("Restarting the script")
+                time.sleep(60)
+        else:
+            if len(dm) > 280:
+                print("DM will be deleted. It's longer than 280 char")
+            elif trigger_word not in dm:
+                print("DM will be deleted. It don't have the trigger word")
+            else:
+                print("DM will be deleted. The sent dm doesn't follow the rules")
+            tw.delete_dm(dm_id)
+            print("Restarting the script")
+            time.sleep(60)
+        messages = list() # resets the dms in messages var
+
+if credential_check and __name__ == '__main__':
     start()
--- bot.py
+++ bot.py
@@ -1,53 +1,103 @@
 import tweepy
 import hidden
 import time
+import requests
+from requests_oauthlib import OAuth1
+import os
+
+
 
 class Twbot:
     def __init__(self):
         self.init = tweepy.OAuthHandler(hidden.CONSUMER_KEY, hidden.CONSUMER_SECRET)
-        self.init.set_access_token(hidden.TOKEY_KEY, hidden.TOKEY_SECRET)
+        self.init.set_access_token(hidden.TOKEN_KEY, hidden.TOKEN_SECRET)
         self.api = tweepy.API(self.init)
     
+    # Testing if the key is valid or not
     def auth_test(self):
         try:
-            self.api.verify_credentials()
-            print('Auth OK')
-        except Exception as err:
-            print(err, 'occured. Re-check the credentials')
+            a = self.api.verify_credentials()
+            return True
+        except tweepy.errors.TweepyException as err:
+            print(err, "occured. Re-check the credentials")
 
+    # Reading through DM
     def read_dm(self):
-        """ Returns 
+        """ Returns https://ton.twitter.com/1.1/ton/data/dm/1461612025017110532/1461611503707045889/Hg6Ziu5X.jpg
         -----------
-        `[{sender, message, message_id}]'
+        `[{sender, message, message_id, media_id, media_url, short_url, media_type}]`
         """
-
         dm = self.api.get_direct_messages()
         dms = list()
         for i in range(len(dm)):
             sender = dm[i].message_create['sender_id']
-            message = dm[i].message_create['message_data']['text']
+            message_data = dm[i].message_create['message_data']
+            message = message_data['text']
             message_id = dm[i].id
-            temp = dict(sender=sender, message=message, message_id=message_id)
-            dms.append(temp)
-        dms.reverse() # reading from the first person to dm
-        return dms
-        """ conditional with media """
+            if 'attachment' not in message_data:
+                temp = dict(sender=sender, message=message, message_id=message_id, media_id=None, media_url=None, short_url=None, media_type=None)
+                dms.append(temp)
 
+            else:
+                attachment_type = dm[i].message_create['message_data']['attachment']['media']['type']
+                attachment_id = dm[i].message_create['message_data']['attachment']['media']['id']
+                attachment_url = dm[i].message_create['message_data']['attachment']['media']['media_url']
+                attachment_short_url = dm[i].message_create['message_data']['attachment']['media']['url']
+                
+                if attachment_type != 'photo':
+                    temp = dict(sender=sender, message=message, message_id=message_id, media_id=None, media_url=None, short_url=None, media_type=attachment_type)
+                    dms.append(temp)
+                else:
+                    media_type = attachment_url.split('.')[-1]
+                    temp = dict(sender=sender, message=message, message_id=message_id, media_id=attachment_id, media_url=attachment_url, short_url=attachment_short_url, media_type=attachment_type)
+                    dms.append(temp)
+        dms.reverse() # Reading from the oldest dm
+        return dms
+
+    # Tweeting tweet (text-only)
     def post_tweet(self, tweet):
         self.api.update_status(tweet)
-        print('Tweeting...')
+        print("Tweeting...")
         time.sleep(3)
-        print('Tweeted!\n')
+        print("Tweeted!")
 
-    def post_tweet_with_media(self, tweet, media):
-        self.api.update_status_with_media(tweet, media)
-        """ CONTINUE """
+    # Uploading image to tweet
+    def upload_image(self, media_url):
+        try:
+            # Authorization for accessing DM's media url
+            oauth = OAuth1(client_key=hidden.CONSUMER_KEY, 
+                            client_secret=hidden.CONSUMER_SECRET, 
+                            resource_owner_key=hidden.TOKEN_KEY, 
+                            resource_owner_secret=hidden.TOKEN_SECRET)
 
+            # Requesting access
+            r = requests.get(media_url, auth=oauth)
+            
+            # Getting the attachment file
+            filename = media_url.split('/')[-1]
+            with open(filename, 'wb') as f:
+                f.write(r.content)
+            
+            # uploading the file
+            media_ids = self.api.media_upload(filename).media_id
+            os.remove(filename)
+            return media_ids
+        except tweepy.errors.TweepyException as err:
+            print(err)
+            pass
+
+    # Tweeting tweet with media
+    def post_tweet_with_media(self, tweet, media_id, media_short_url):
+        tweet = tweet.replace(media_short_url, '')
+        self.api.update_status(tweet, media_ids=media_id)
+        print("Tweeting with attachment(s)...")
+        time.sleep(3)
+        print("Tweeted with attachment(s)!")
+
+    # Deleting tweet
     def delete_dm(self, id):
         self.api.delete_direct_message(id)
-        print('Deleting DM...')
+        print("Deleting DM...")
         time.sleep(3)
-        print('DM deleted!\n')
-
-# post tweet > API.update_status()
-# post tweet with media > API.update_status_with_media()
+        print("DM deleted!")
+        print("-"*35)
